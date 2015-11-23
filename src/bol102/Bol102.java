e3bb3e97e078b8e711421ff017f66aa9c3652220
Pablite5 <Pablite5@GAMING>
2015-11-23 09:57:23 +0100

    63

---
+++ src/bol102/Bol102.java
@@ -0,0 +1,17 @@
+
+package bol102;
+
+/**
+ *
+ * @author Pablite5
+ */
+public class Bol102 {
+
+    
+    public static void main(String[] args) {
+        Calculo obx=new Calculo();
+        obx.SetAdivinar();
+        obx.ejecutarJuego();
+    }
+    
+}
